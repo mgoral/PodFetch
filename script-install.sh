86f341fb61b13b5ab69f2912c52f61e8fbd2c964
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-05-12 16:46:59 +0200

    Do yes.

--- script-install.sh
+++ script-install.sh
@@ -4,4 +4,4 @@ env ACCEPT_EULA=Y apt upgrade
 apt-get purge gcc-5 gcc-5-base gcc-6-base
 apt install gcc-multilib
 
-apt clean && apt -o DPkg::Options::='--force-confnew'  --assume-yes install gcc libssl-dev gcc-multilib musl musl-dev musl-tools
+apt clean && apt -o DPkg::Options::='--force-confnew'  --assume-yes install gcc libssl-dev musl musl-dev musl-tools
